88dfcae4d637d7976d804ad92c09c58469f4efc0
Diego Orlando <diego.orlando@accendoit.com>
2020-11-03 22:51:39 -0300

    Cubo con moviemientos y cancha

---
+++ Super Tennis/Assets/Scripts/Game/ActionMapper.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionMapper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static bool GetMoveLeft()
+    {
+        return Input.GetKey(KeyCode.LeftArrow);
+    }
+
+    public static bool GetMoveRight()
+    {
+        return Input.GetKey(KeyCode.RightArrow);
+    }
+    public static bool GetMoveUp()
+    {
+        return Input.GetKey(KeyCode.UpArrow);
+    }
+
+    public static bool GetMoveDown()
+    {
+        return Input.GetKey(KeyCode.DownArrow);
+    }
+}
---
+++ Super Tennis/Assets/Scripts/Game/PlayerMover.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    public int speed = 5;
+    public CharacterController CharacterController;
+
+    private int xMovement;
+    private int zMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ReadInput();
+        UpdatePosition();
+    }
+
+    private void ReadInput()
+    {
+        if (ActionMapper.GetMoveLeft())
+            xMovement = -1;
+        else if (ActionMapper.GetMoveRight())
+            xMovement = 1;
+        else
+            xMovement = 0;
+
+        if (ActionMapper.GetMoveDown())
+            zMovement = -1;
+        else if (ActionMapper.GetMoveUp())
+            zMovement = 1;
+        else
+            zMovement = 0;
+    }
+
+    private void UpdatePosition()
+    {
+        float x = Time.deltaTime * speed * xMovement;
+        float y = 0;
+        float z = Time.deltaTime * speed * zMovement;
+        Vector3 newPosition = new Vector3(x, y, z);
+        CharacterController.Move(newPosition);
+    }
+}
