dc87a036e887b0b29f5bc78918f3a008f1ead3da
Diego Orlando <diego.orlando@accendoit.com>
2020-11-09 17:39:53 -0300

    Main Menu

---
+++ Super Tennis/Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [HideInInspector]
+    public GameMode gameMode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    public enum GameMode
+    {
+        Exhibition,
+        Tournament
+    }
+
+    public void PlayExhibition()
+    {
+        gameMode = GameMode.Exhibition;
+    }
+
+    public void PlayTournament()
+    {
+        gameMode = GameMode.Tournament;
+    }
+
+    public void PlayUSOpen()
+    {
+        SceneManager.LoadScene("USOpen");
+    }
+
+    public void PlayRolandGarros()
+    {
+        SceneManager.LoadScene("RolandGarros");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
